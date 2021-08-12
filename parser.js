c48847024134cdcef30a67eb8c6443820bc99772
Pavel Zarubin <pavel@orendat.ru>
2021-08-13 01:30:07 +0300

    Fixes

--- parser.js
+++ parser.js
@@ -56,47 +56,60 @@ async function preparePage(page) {
     await page.goto(base_url);
     const cookies = [
         {
-            'name': 'abp',
-            'value': '0'
-        }, {
-            'name': 'st',
-            'value': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoidko0TkJ5SlU2TElJTGRDY3VNTkdBYnlzVGk3VTVKRW8wVkZQNXBSVll0ZVlpM0lxVUJ1Q2JQVUtjS0FnSGJvRW9yK3ZEVDlRd2Q1cUZ3dUtTa1RpVXNuLys5ZGVQenc0ZUNKanBSWWtFK1ozMmRTSlFEeDRUbmQvcE1Nc25Vckw0Q0liRERXM3pWL2s2UHA2UlpmWVM3SDFpdVViRkJjQkMzSjBLNzMwNmE0WDRpMDQ3VGRHNFd0a2piRm8yRWNkMTRzRkJLN1V5V0h0Q2lpR0lubmFmMDZzV2FsRVFNV3hiVVBWaUtKUmV5VHN6VUxUMnFZcVZVSlc1OU5RbVJiQy84KzAzbER1Vnlpa3poZEhoYTdHbkY3NE5ERVd1bndibGVjMDhQbm52UkE9IiwiaWF0IjoxNjI4NzI0MDE3LCJleHAiOjE2Mjk5MzM2MTd9.g4RiWGLx15eagntrSDgmh8FrC5V7BdnKE0JXZjYngzE'
-        }, {
-            'name': 'no-ssr',
-            'value': '1'
-        }, {
-            'name': 'isWideScreen',
-            'value': '1'
-        }, {
-            'name': 'cto_bundle',
-            'value': 'dE4Dtl9NdllFSSUyQnczWTUxc2ZPM3NtN0lWeFhHeWVsQ1VMUEVMcjVVeng3dlM5RnlQN2hTc2lDMlNwUCUyQlAlMkJITjFkdHRVMFV3dGpLM2FrYUpZeHJ1T0lldkgzTlhtc09ncEY2VURPM0F2ZmhkZTBkSU90elVQaTFNZWFEWE1zbFZhbmVpag'
-        }, {
             'name': 'sx',
-            'value': 'H4sIAAAAAAACA1XMsQrDMAwE0H%2FRnEFpha3kb2qRiGCK3AqsQsi%2F10uHbgd3907glHvrpe3M4WrupGyuwbCe0GEFKzmW%2FinvFi7kFCjiGOKmrqIwwQbrnG7MM9IdrwlGjpIXOfbKymKoMl5V8UdS3fKTXu14IA1J2EY9pgN1I%2F8nU7quL2%2BxhKKnAAAA'
-        }, {
+            'value': 'H4sIAAAAAAACA52TQXbqMAxF95JxB4qjJEp3Q5QgQIADKhHQ071%2FpefQwvB35oF9bb37%2FFlQ087T3E9rIjfJZiiUTZyK989iLt6L3XjbIZ539x4BM4qpEiMYCQmAF2%2FFWLyXTSLqKmyar7ei3lzvU93c5psHkhiCqYxZHsiPY%2BrsmJt6NM%2BmopQRGVQyZLJnJLTdN7KsrnevSii1RWIBQUQ1cn4gV9Bpfbrki2YkFYr7UONKBxJUeEZ2DZaBvG9O683HYWtXV48TmJXU0R7EXGU15u60MnTjjOxG7KDAJKxPxLJMqQ1irL1vO96uNcLhDMIY%2BwUeSCyPcrmcN6vSwCJHJ%2BN4r5oZiPFrlHUXyA567ra2m2Z295hJMi0z4QM5rbb1ptV8Dznq8crYZZYhkgfyFzslUBXI%2Fjwd79aDrMOlszARose5nyj7Ia3bsq4HWncV85DqxE3NBBXA2HOVUrVa9fUzO1WAC%2Ftwbud2z9PRMSYSRY5sXX40NcMmrorxD%2BBLzcAzk3%2BbFIX8ikwUyHThkio4pkERwmwEx%2BLZfzytT7ZL17bfnATDiwU1%2BhE6ncIbviKrxdN4bc886sdcRu6hmMI8Sf4tk243c62Hal9HmlkAWeJ1ysyGmV6R3benAepmhnQOWFQ%2BuhzTUazlT8hmeeXgu2rcj7nfs2mONlmOqgT7T8h2qXx7q5EO9%2B2pF1m%2BBmQAA4%2Fc%2F4RcPiZP3dT0Oux69wwcSebln%2FwW9L%2BQlL6%2B%2FgFtWwb2kwQAAA%3D%3D'
+        },
+        {
             'name': 'f',
-            'value': '5.0c4f4b6d233fb90636b4dd61b04726f147e1eada7172e06c47e1eada7172e06c47e1eada7172e06c47e1eada7172e06cb59320d6eb6303c1b59320d6eb6303c1b59320d6eb6303c147e1eada7172e06c8a38e2c5b3e08b898a38e2c5b3e08b890df103df0c26013a0df103df0c26013a2ebf3cb6fd35a0ac0df103df0c26013a8b1472fe2f9ba6b978e38434be2a23fac7b9c4258fe3658dbf11f980bc2bc377f2c082410b22639b04dbcad294c152cb0df103df0c26013a20f3d16ad0b1c5462da10fb74cac1eab2da10fb74cac1eab2ebf3cb6fd35a0acf0c77052689da50d03c77801b122405c2da10fb74cac1eab2da10fb74cac1eab71e7cb57bbcb8e0fba0ac8037e2b74f903c77801b122405cfa967f4e8cd565314e1848a6933553220823d89c0e0d484fbb30ebf890d750560e79b44b1f01e1bea925d90e8dfec722c4a7bdb682b087dd38f0f5e6e0d2832edc302a2ed09356c57c6c9b2f606103d746b8ae4e81acb9fa46b8ae4e81acb9fa02c68186b443a7ac15e402c38704f6255881802286afa4012da10fb74cac1eab2da10fb74cac1eab25037f810d2d41a8134ecdeb26beb8b53778cee096b7b985bf37df0d1894b088'
-        }, {
-            'name': 'uxs_uid',
-            'value': 'cc8aab60-fafa-11eb-8e22-b74e16365b4e'
-        }, {
-            'name': 'dfp_group',
-            'value': '62'
-        }, {
-            'name': 'ft',
-            'value': '"D3/WPWVSaUdaKdwvwFtkwwnLdQZhB/N3IjBljktZuIW3CRm+YaQR1FfhBazvO8I0H9ZcQu6JRdzWz/XUh3NSC/BU728KaIW2s6BwVmdhKcRibNhC9zgkubankb7m9l6eoRvdMP1VRNu4KETAof+Ktd9xA1JxLJxaA7jqSx3BkztQgUZlu4k3mYrab/v3T8vu"'
-        }, {
+            'value': '5.0c4f4b6d233fb90636b4dd61b04726f147e1eada7172e06c47e1eada7172e06c47e1eada7172e06c47e1eada7172e06cb59320d6eb6303c1b59320d6eb6303c1b59320d6eb6303c147e1eada7172e06c8a38e2c5b3e08b898a38e2c5b3e08b890df103df0c26013a0df103df0c26013a2ebf3cb6fd35a0ac0df103df0c26013a8b1472fe2f9ba6b978e38434be2a23fac7b9c4258fe3658dbf11f980bc2bc377f2c082410b22639b04dbcad294c152cb0df103df0c26013a20f3d16ad0b1c5462da10fb74cac1eab2da10fb74cac1eab2ebf3cb6fd35a0acf0c77052689da50d03c77801b122405c2da10fb74cac1eab2da10fb74cac1eab71e7cb57bbcb8e0fba0ac8037e2b74f903c77801b122405cfa967f4e8cd565314e1848a6933553220823d89c0e0d484fbb30ebf890d750560e79b44b1f01e1bea925d90e8dfec722c4a7bdb682b087dd38f0f5e6e0d2832edc302a2ed09356c57c6c9b2f606103d746b8ae4e81acb9fa46b8ae4e81acb9fa02c68186b443a7ac15e402c38704f625ad217318ef88493d2da10fb74cac1eab2da10fb74cac1eab25037f810d2d41a8134ecdeb26beb8b53778cee096b7b985bf37df0d1894b088'
+        },
+        {
+            'name': 'v',
+            'value': '1628803296'
+        },
+        {
+            'name': 'buyer_index_tooltip',
+            'value': '1'
+        },
+        {
             'name': 'u',
-            'value': '2oufkbnz.1kh7th.tqjqjqs1osg'
-        }, {
-            'name': '_buzz_fpc',
-            'value': 'JTdCJTIycGF0aCUyMiUzQSUyMiUyRiUyMiUyQyUyMmRvbWFpbiUyMiUzQSUyMi53d3cuYXZpdG8ucnUlMjIlMkMlMjJleHBpcmVzJTIyJTNBJTIyVGh1JTJDJTIwMTElMjBBdWclMjAyMDIyJTIwMjMlM0EyMCUzQTI2JTIwR01UJTIyJTJDJTIyU2FtZVNpdGUlMjIlM0ElMjJMYXglMjIlMkMlMjJ2YWx1ZSUyMiUzQSUyMiU3QiU1QyUyMnZhbHVlJTVDJTIyJTNBJTVDJTIyZTZkNjYxZjA4Yzg5NTgxZjNjNjk3MzM5YTBlYWU2MmM3NDkwNDAxMTllZDM5MTIwYTU5MmU5ODk4NDEyYzE4NyU1QyUyMiUyQyU1QyUyMmlzTmV3Q29va2llJTVDJTIyJTNBdHJ1ZSU3RCUyMiU3RA=='
-        }, {
+            'value': '2ot1hf3x.cx8ex3.1n8h2u8j8c4w0'
+        },
+        {
             'name': 'sessid',
-            'value': 'e90862e19e1abce27308214c93807979.1628724030'
-        }, {
-            'name': '__gads',
-            'value': 'ID=1f6e8a9edbe3d094-2279b38e9dc800c4:T=1628724018:S=ALNI_MZHSmxiHtKji8g1hLPnmLbFDFL8jw'
+            'value': '5ac32fb2da935eff1c4601bfb8cea44f.1628805904'
+        },
+        {
+            'name': 'buyer_laas_location',
+            'value': '637640'
+        },
+        {
+            'name': 'buyer_local_priority_v2',
+            'value': '0'
+        },
+        {
+            'name': 'so',
+            'value': '1628807066'
+        },
+        {
+            'name': 'ft',
+            'value': '"G2KGysluTw8EquYLLxm5S+pglQIQCkpcaPKScMgSqmrYaKtUrPbVJq2tuTKe8oHHDGtQZ13hBw3WKySwTfCrLLHfR37BiJd+YMDvR3g1+G27eMFjx13BI3qn1lkS/gNN7cbMglAF3rnNZrJwOvwTOC24XBNgbiw3/uHq23lRfbnYD+m9vnlCwxqm57s6jtnf"'
+        },
+        {
+            'name': 'buyer_selected_search_radius0',
+            'value': '0'
+        },
+        {
+            'name': 'luri',
+            'value': 'moskva'
+        },
+        {
+            'name': 'isLegalPerson',
+            'value': '0'
+        },
+        {
+            'name': 'buyer_location_id',
+            'value': '637640'
         }
     ];
 
